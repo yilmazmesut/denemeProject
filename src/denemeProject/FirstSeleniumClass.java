5c9224c276e1814a9d39c8d9037a837e98534d16
yilmazmesut <yilmazmesut.uk@gmail.com>
2021-10-01 17:25:29 +0100

    Initial commit

---
+++ src/denemeProject/FirstSeleniumClass.java
@@ -0,0 +1,12 @@
+package denemeProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSeleniumClass {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:/Users/Mesut/Documents/selenium dependencies/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com");
+    }
+}
